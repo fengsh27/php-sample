5c17808f86e0ccfabe2dabf60b9ba90e0e68a9bc
fengsh27 <123769183+fengsh27@users.noreply.github.com>
2023-02-17 17:20:23 -0500

    demo how to use docker compose to manage php server (#1)
    * add docker compose file
    
    * pass port to php container

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 
-
 $method = $_SERVER['REQUEST_METHOD'];
 $uri = $_SERVER['REQUEST_URI'];
 $protocol = $_SERVER['SERVER_PROTOCOL'];
@@ -11,6 +10,16 @@ foreach ($headers as $key => $header) {
     echo "$key: $header <br/>";
 }
 
-
+$host = getenv("MYSQL_HOST");
+$port = getenv("MYSQL_PORT");
+$host = "$host:$port";
+$user = getenv("MYSQL_USER");
+$pass = getenv("MYSQL_PASSWORD");
+$conn = new mysqli($host, $user, $pass);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "Connected to MySql server successfully";
+}
 
 ?>
